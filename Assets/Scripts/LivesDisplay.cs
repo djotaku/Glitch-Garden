f09870ac422f3ddd9ff19bb588a17d9dd47aa05a
Eric Mesa <ericsbinaryworld@gmail.com>
2019-11-23 19:33:56 -0500

    Set volume and difficulty
    And also fixed a bug with the win

--- Assets/Scripts/LivesDisplay.cs
+++ Assets/Scripts/LivesDisplay.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 
 public class LivesDisplay : MonoBehaviour
 {
-    [SerializeField] int lives = 5;
+    [SerializeField] float baseLives = 3;
     [SerializeField] int damage = 1;
+    
+    float lives;
     Text livesText;
 
 
     void Start()
     {
+        lives = baseLives - PlayerPrefsController.GetDifficulty();
         livesText = GetComponent<Text>();
         UpdateDisplay();
     }
